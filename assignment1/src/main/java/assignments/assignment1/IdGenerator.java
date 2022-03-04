ebb4106004993089fc3bce48c6c2e02454036f50
abby.marvel <abby.marvel@ui.ac.id>
2022-03-04 09:01:36 +0700

    mengumpulkan TP1

--- assignment1/src/main/java/assignments/assignment1/IdGenerator.java
+++ assignment1/src/main/java/assignments/assignment1/IdGenerator.java
@@ -1,5 +1,6 @@
 package assignments.assignment1;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -84,8 +85,37 @@ public class IdGenerator {
      * Parameter dan return type dari method ini tidak boleh diganti
      */
     public static String generateId(String programStudi, String angkatan, String tanggalLahir){
-        // TODO: Tuliskan implementasi untuk membuat ID keanggotaan perpustakaan
-        return "";
+        // Beberapa line dibawah ini untuk melakukan validasi input (if statement)
+        String[] prodi = {"SIK", "SSI", "MIK", "MTI", "DIK"};
+        if (angkatan.length() != 4 || !Arrays.asList(prodi).contains(programStudi)
+            || 2000 >= Integer.parseInt(angkatan) || Integer.parseInt(angkatan) > 2021
+                || !tanggalLahir.matches("[0-9]{2}/[0-9]{2}/[0-9]{4}")) {
+            return "Input tidak valid!";
+        }
+
+        // Menyiapkan 11 char pertama untuk menghitung Checksum C dan Checksum K
+        String id = programStudi + angkatan.substring(2,4) + tanggalLahir.substring(0,2)
+                + tanggalLahir.substring(3,5) + tanggalLahir.substring(8,10);
+
+        int j = 11;
+        int c = 0;
+        for (int i = 0, n = id.length(); i < n; i++, j--) {         // For loop digunakan untuk menghitung Checksum C
+            c += (getValueFromChar(id.charAt(i)) * j);
+        }
+        c %= 36;
+        char checkSumC = getCharFromValue(c);
+        id += checkSumC;
+
+        int y = 12;
+        int k = 0;
+        for (int x = 0, n = id.length(); x < n; x++, y--) {         // For loop digunakan untuk menghitung Checksum K
+            k += (getValueFromChar(id.charAt(x)) * y);
+        }
+        k %= 36;
+        char checkSumK = getCharFromValue(k);
+        id += checkSumK;
+
+        return "ID Anggota: " + id;
     }
 
     /*
@@ -93,7 +123,26 @@ public class IdGenerator {
      * Parameter dan return type dari method ini tidak boleh diganti
      */
     public static boolean checkValidity(String idAnggota) {
-        // TODO: Tuliskan implementasi untuk mengecek validitas ID keanggotaan perpustakaan
-        return false;
+        if (idAnggota.length() != 13) {         // Memastikan idAnggota memiliki 13 char
+            return false;
+        }
+        int j = 11;
+        int c = 0;
+        for (int i = 0, n = 11; i < n; i++, j--) {
+            c += (getValueFromChar(idAnggota.charAt(i)) * j);
+        }
+        c %= 36;
+        char checkSumC = getCharFromValue(c);
+
+        int y = 12;
+        int k = 0;
+        for (int x = 0, n = 12; x < n; x++, y--) {
+            k += (getValueFromChar(idAnggota.charAt(x)) * y);
+        }
+        k %= 36;
+        char checkSumK = getCharFromValue(k);
+
+        // Jika digit ke-11 sama dengan checkSumC dan digit ke-12 sama dengan checkSumK maka validitas bernilai true
+        return idAnggota.charAt(11) == checkSumC && idAnggota.charAt(12) == checkSumK;
     }
 }
